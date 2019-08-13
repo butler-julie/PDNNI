ed22d798f4721bb85c19551cdc639c518f377fbc
Julie Hartley <51090497+jhartley1121@users.noreply.github.com>
2019-08-13 16:41:13 -0400

    Update Main.py

--- Code/Main.py
+++ Code/Main.py
@@ -1,21 +1,56 @@
-from NeuralNetork import Trainer, Restore
+##################################################
+##################################################
+
+##################################################
+##################################################
+
+##############################
+# IMPORTS
+##############################
+# LOCAL IMPORTS
+from NeuralNetork import Trainer
 from SRGNeuralNetwork import SRGNeuralNetwork
 
-def main():
+def train_and_predict_eigen_srg(nn_specs, save_names, prediction_values):
+    """
+        Inputs:
+            nn_specs (a list of length 8): Contains the following specifications for the neural network in order:
+                number of hidden layers, number of neurons per hiddne layer, learning rate, input data dimension,
+                output data dimension, file location of input data, file location of training data, number of 
+                training iterations
+            save_names (a list of length 2): Contains the following names in order: the location to save/load
+                the trained weights, the location to save/load the trained biases
+            
+    """
     print("Initializing Neural Network")
-    srg_train = Trainer(1, 375, 0.1, 1, 36, 'input_pm_ds_1.npy', 'SRG_PM/SRG_PM_4_DS_1.npy')
+    srg_train = Trainer(nn_specs[0], nn_secs[1], nn_specs[2], nn_specs[3], nn_specs[4], nn_specs[5], nn_specs[6])
+    
     print ("Training and Saving Weights and Biases")
-    srg_train.run(3000, 'results_pm/weights_4_1', 'results_pm/biases_4_1')
-    w = srg_train.get_weights()
-    print(len(w))
-    print(srg_train.get_loss())
-    H0 = np.load('SRG_PM/SRG_PM_4_DS_1e-1.npy')[0]
+    srg_train.run(nn_specs[7], save_names[0], save_names[1])
+    
+    print('\nLoss: ', srg_train.get_loss(), '\n')
+    
+    H0 = np.load(nn_specs[6])[0]
     H0 = np.reshape(H0, (6, 6))
     true = eigvalsh(H0)
+    
     print ("Restoring Neural Network")
-    srg = SRGNeuralNetwork('results_pm/weights_4_1.npy', 'results_pm/biases_4_1.npy')
-    print(srg.compare_eigenvalues([[1.01], [5.01], [10.01], [100]], true))
+    srg = SRGNeuralNetwork(save_names[0], save_names[1])
+    
+    print(srg.compare_eigenvalues(prediction_values, true))
+    
+def predict_eigen_srg (training_file, save_names, prediction_values):
+    H0 = np.load(training_file)[0]
+    H0 = np.reshape(H0, (6, 6))
+    true = eigvalsh(H0)
+    
+    print ("Restoring Neural Network")
+    srg = SRGNeuralNetwork(save_names[0], save_names[1])
+    
+    print(srg.compare_eigenvalues(prediction_values, true))
 
+def main():
+    print('Main')
 
 if __name__=='__main__':
     main()
