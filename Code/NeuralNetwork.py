a9a86b40282f66b6b080bb5f783a9f0ed2b6df8c
Julie Hartley <51090497+jhartley1121@users.noreply.github.com>
2019-08-13 15:04:15 -0400

    Adding comments

--- Code/NeuralNetwork.py
+++ Code/NeuralNetwork.py
@@ -1,89 +1,115 @@
 #################################################
-# Universal Approximator Matrices
+# Neural Network
 # Julie Hartley
 # Version 0.0.5
 # August 8, 2019
 #
-# A universal approximator for data produced by the SRG
-# and the IMSRG
+# Basic code for a neural network that allows the
+# trained weights and biases to be saved and 
+# used later.
 #################################################
 
 #################################################
 # OUTLINE
 # 
+# Trainer
+#
+# Restore
 #################################################
 #############################
 # IMPORTS
 #############################
-# SYSTEM IMPORTS
-import time, os, sys
-dir = os.path.dirname (os.path.realpath (__file__))
-# For timing
-import timeit
-
 # THIRD-PARTY IMPORTS
 # For machine learning
 import tensorflow as tf
-
 # For calculations
 import numpy as np
-
-# For graphing
-#import matplotlib.pyplot as plt
-#from pylab import *
-
-# For SRG and matrix manipulation
-# Dor product and matrix/array manipulation
-from numpy import array, dot, diag, reshape
-# Eigenvalue, eigenvector solver
-from scipy.linalg import eigvalsh
-# ODE solver
-from scipy.integrate import ode
-
-# Creating random numbers (for file naming)
-import random
 from math import sqrt
-
 # LOCAL IMPORTS 
-# Local imports are stored in the common code repository
-# The neural network
+# The neural network for Trainer
 from NeuralNetworkSupport import neural_network as ua
 
 class Trainer:
+    """
+        Trains a neural network based on spefications given in the initialization.  Allows for
+        weights and biases of the trained neural network to be viewed and saved.
+    """
     # __INIT__
     def __init__ (self, hidden_layers, hidden_neurons, learning_rate, input_dim, output_dim,
         input_file, training_file):
+        """
+            Inputs:
+                hidden_layers (an int): the number of hidden layers in the neural network
+                hidden_neurons (an int): the number of neurons per hidden layer
+                learning_rate (a float): the learning rate for the optimizer (typically less
+                    than one)
+                input_dim (an int): the dimension of the input data
+                output_dim (an int): the dimension of the training data
+                input_file (a str): the location where the input data is saved (must be .npy extension)
+                training_file (a str): the location where the training data is saved (must be .npy extension)
+        """
         self.hidden_layers = hidden_layers
         self.hidden_neurons = hidden_neurons
         self.learning_rate = learning_rate
         self.input_dim = input_dim
         self.output_dim = output_dim
-        self.output_dim_sqrt = sqrt(self.output_dim)
         self.input_data = np.load(input_file)
         self.training_data = np.load(training_file)
         self.weights = []
         self.biases = []
         self.loss = 'NaN'
+        self.isTrained = False
     
     # GET_TENSOR_NUMERIC
     def get_tensor_numeric (self, name):
+        """
+            Inputs:
+                name (a str): The name of the tensor
+            Returns:
+                Unnamed: The numeric value of the tensor (either a single number
+                    or a numpy array
+            Gets the numeric value of a specific Tensorflow tensor, specified by name.
+        """
         return tf.get_default_graph().get_tensor_by_name(name).eval()
 
+    # GET_WEIGHTS
     def get_weights(self):
-        return self.weights
+        """"
+            Returns:
+                self.weights (a numpy array): the trained weights
+            Returns the trained weights of the neural network (if it has been trained).
+        """
+        if isTrianed:
+            return self.weights
+        else:
+            print ("Neural Network is not yet trained.")
 
+    # SAVE_WEIGHTS
     def save_weights(self, filename):
         np.save(filename, self.weights)
 
+    #GET_BIASES
     def get_biases(self):
+         """"
+            Returns:
+                self.biases (a numpy array): the trained biases
+            Returns the trained biases of the neural network (if it has been trained).
+        """
+        if isTrianed:
+            return self.biases
+        else:
+            print ("Neural Network is not yet trained.")
         return self.biases
 
+    # SAVE_BIASES
     def save_biases(self, filename):
         np.save(filename, self.biases)
 
+    # GET_LOSS
     def get_loss(self):
         return self.loss
 
+    # GET_DIMS
     def get_dims(self):
         return self.hidden_layers, [self.input_dim, self.hidden_neurons, self.output_dim]
 
@@ -134,6 +160,7 @@ class Trainer:
                     train_optimizer], feed_dict = { input_values:self.input_data,
                      output_values:self.training_data})
             
+            self.isTrained = True
             self.loss = current_loss
             for i in range(1, self.hidden_layers+1):
                 name = "Graph/weights_" + str(i) + ":0"
@@ -143,7 +170,8 @@ class Trainer:
 
             self.weights.append(self.get_tensor_numeric("Graph/weights_output_layer:0"))
 
-    def run (self, iterations, weights_file, biases_file):
+    # TRAIN_AND_SAVE
+    def train_and_save (self, iterations, weights_file, biases_file):
         self.train(iterations)
         self.save_weights(weights_file)
         self.save_biases(biases_file)
@@ -151,6 +179,7 @@ class Trainer:
 
 
 class Restore:
+    # __INIT__
     def __init__ (self,weights_file, biases_file):
         self.weights = np.load(weights_file, allow_pickle=True)
         self.biases = np.load(biases_file, allow_pickle=True)
@@ -180,60 +209,6 @@ class Restore:
         #print(len(final))
         return final
         
-
+    # PREDICT
     def predict (self, prediction_value):
         return self.restore_NN(prediction_value)
-
-        
-     
-
-
-
-
-
-"""
-
-
-
-# Using the neural network to predict values of the SRG flow equation
-
-# The values of s to calculate matrices at
-prediction_values = np.arange(0, 10.2, 0.01)
-prediction_values = prediction_values.reshape (len(prediction_values), 1)
-
-# Dummy variable for prediction algorithm        
-zeros = np.zeros([10**2])
-dummy_output = []
-dummy_output.append (zeros)
-
-# Use the trained neural network to make the predictions
-y_true_results, y_approximate_results = sess.run ([y_true, y_approximate], 
-    feed_dict={input_values:prediction_values, output_values:dummy_output})
-
-true = eigvalsh(reshape(self.training_data[0], (10,10)))
-predict_ml = np.sort(diag(reshape(y_approximate_results[-1], (10,10))))
-N = len(true)
-
-ml_error = np.sum((true-predict_ml)**2)/N
-
-print ('ML Error ', ml_error, '\n')
-"""
-
-"""       
-)
-
-
-    def prediction_error_analysis(self, prediction_values):
-        true_result = eigvalsh(reshape(self.H0, (self.output_dim_sqrt,self.output_dim_sqrt)))
-        y_true_results, y_approximate_results = predict(prediction_values)
-
-        predict_ml = get_eigenvalues(y_approximate_result)
-        predict_ode = get_eigenvalues(y_true_results)
-
-        N = len(true)
-
-        ml_error = MSE(true_result, predict_ml)
-        ode_error = MSE(true_result, predict_ode)
-    
-        return ml_error, ode_error
-"""
