2085345b2302506a74079cf148782b509447a669
Julie Hartley <51090497+jhartley1121@users.noreply.github.com>
2019-08-13 15:17:00 -0400

    Added comments

--- Code/Support.py
+++ Code/Support.py
@@ -1,7 +1,35 @@
+##################################################
+# Support
+# Julie Hartley
+# Version 1.0.0
+# August 13, 2019
+#
+# Contains support methods that are or could be needed
+# by other files in PDNNI
+##################################################
 
+##################################################
+# OUTLINE
+#
+# print_dims(x): Prints the dimensions of a 2D square array.
+#
+# get_dims(x): Returns the dimensions of a 2D square array.
+##################################################
 
+# PRINT_DIMS
 def print_dims (x):
+    """
+        Inputs:
+            x: a 2D square array or list
+        Prints the dimensions of a 2D square array.
+    """
     print(len(x), len(x[0]))
 
+# GET_DIMS
 def get_dims(x):
+    """
+        Inputs:
+            x: a 2D square array or list
+        Returns the dimensions of a 2D square array.
+    """
     return len(x), len(x[0])
