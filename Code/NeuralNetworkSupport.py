24caccb5da9e909f60345bf01d2bb0f7b685fd86
Julie Hartley <51090497+jhartley1121@users.noreply.github.com>
2019-08-13 15:29:45 -0400

    Update NeuralNetworkSupport.py

--- Code/NeuralNetworkSupport.py
+++ Code/NeuralNetworkSupport.py
@@ -4,18 +4,8 @@
 # February 20, 2019
 # Version 0.2
 #
-# A collection of functions for setting up and running neural networks.  Many are 
-# related to the use of neural networks as universal function approximators.
-
-# FUNCTIONS:
-# initilize_neural_network:  Creates the weights and biases of a neural network, for any
-#   number of hidden layers given in an inputted array of seed values for the weights and
-#   biases.
-# xavier_initialization: initializes the weights of a neural network using the Xavier
-#   initialization method
-# universal_function_approximator: approximates a function of any input and output 
-#   dimensions using a one hidden layer neural network with a specified number of 
-#   neurons.
+# A neural network created in Tensorflow.  It can have any number of hidden layers but 
+# each hidden layer must have the same number of neurons
 ########################################################################################
 
 #############################
@@ -67,44 +57,3 @@ def neural_network (input_vector, input_dim, hidden_dim, output_dim, num_hidden_
     z = tf.matmul (activated_function, weights_output_layer)
         
     return z
-"""
-PANIC VERSION -- WORKS WITH 2 HIDDEN LAYERS
-# UNIVERSAL_FUNCTION_APPROXIMATOR_N_HIDDEN_LAYERS
-def universal_function_approximator_N_hidden_layers (input_vector, input_dim, hidden_dim, output_dim, num_hidden_layers, lower_bound, upper_bound):
-        # First Hidden Layer
-        weights_hidden_layer = tf.get_variable (name='weights_hidden_layer', 
-            shape=[input_dim, hidden_dim],
-            initializer=tf.random_normal_initializer(stddev=0.1))
-        
-        biases_hidden_layer = tf.get_variable (name='biases_hidden_layer', 
-            shape=[hidden_dim],
-            initializer=tf.constant_initializer(0.0))
-        
-        z = tf.matmul (input_vector, weights_hidden_layer) + biases_hidden_layer
-        activated_function = tf.nn.relu (z)
-
-        #Second Hidden Layer
-        weights_hidden_layer_1 = tf.get_variable (name='weights_hidden_layer_1', 
-            shape=[hidden_dim, hidden_dim],
-            initializer=tf.random_normal_initializer(stddev=0.1))
-        
-        biases_hidden_layer_1 = tf.get_variable (name='biases_hidden_layer_1', 
-            shape=[hidden_dim],
-            initializer=tf.constant_initializer(0.0))
-        
-        z = tf.matmul (activated_function, weights_hidden_layer_1) + biases_hidden_layer_1
-        activated_function = tf.nn.relu (z)
-
-        #Output Layer
-        weights_output_layer = tf.get_variable (name='weights_output_layer', 
-            shape=[hidden_dim, output_dim],
-            initializer=tf.random_normal_initializer(stddev=0.1))
-            
-        # See notebook #1 page 22
-        z = tf.matmul (activated_function, weights_output_layer)
-        
-        return z
-"""   
-
- 
-    
