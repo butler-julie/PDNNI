8e4dedc35ce913800c58dc9c268fd874bc192c01
Julie Hartley <51090497+jhartley1121@users.noreply.github.com>
2019-08-19 11:18:55 -0400

    Added comments

--- Code/NeuralNetworkSupport.py
+++ Code/NeuralNetworkSupport.py
@@ -8,6 +8,12 @@
 # each hidden layer must have the same number of neurons
 ########################################################################################
 
+########################################################################################
+# OUTLINE:
+# neural_network (input_vector, input_dim, hidden_dim, output_dim, num_hidden_layers):
+# Initializes a neural network using Tensorflow and then is used for the training process.
+########################################################################################
+
 #############################
 #                           #
 # IMPORTS                   #
@@ -19,19 +25,38 @@ import tensorflow as tf
 # For matrix calculations
 import numpy as np
 
+# NEURAL_NETWORK
 def neural_network (input_vector, input_dim, hidden_dim, output_dim, num_hidden_layers):
+    """
+        Inputs:
+            input_vector (a Tensor): the input value of the neural netowrk
+            input_dim (an int): the dimension of the input
+            hidden_dim (an int): the number of neurons per hidden layer (must be the same per 
+                hidden layer)
+            output_dim (an int): the dimension of the output of the neural network
+            num_hidden_layers (an int): the number of hidden layers
+        Returns:
+            z (an Tensor): the output of the neural network
+        Initializes a neural network using Tensorflow and then is used for the training process.
+    """
     # First Hidden Layer
+    # weights of first hidden layer (initialized to random numbers at the beginning)
+    # Names of the weights and biases are important for lookup later
     weights_first_hidden_layer = tf.get_variable (name='weights_1', 
         shape=[input_dim, hidden_dim],
         initializer=tf.random_normal_initializer(stddev=0.1))
-        
+    # biases of the first hidden layer (initialized to zero at the beginning)
     biases_first_hidden_layer = tf.get_variable (name='biases_1', 
         shape=[hidden_dim],
         initializer=tf.constant_initializer(0.0))
         
+    # multiply input vector by the weights and add the biases
+    # matmul = Matrix Multiplication
     z = tf.matmul (input_vector, weights_first_hidden_layer) + biases_first_hidden_layer
+    # apply the activation function (relu(x) = max(0,x)
     activated_function = tf.nn.relu (z)
 
+    # Do the same thing for each of the other hidden layers
     for i in range (1, num_hidden_layers):
         # Interior Hidden Layers
         weight_name = 'weights_' + str(i+1)
@@ -48,7 +73,8 @@ def neural_network (input_vector, input_dim, hidden_dim, output_dim, num_hidden_
         z = tf.matmul (activated_function, weights_hidden_layer_1) + biases_hidden_layer_1
         activated_function = tf.nn.relu (z)
 
-    #Output Layer
+    # Output Layer
+    # Output layer does not have biases or activation function by design
     weights_output_layer = tf.get_variable (name='weights_output_layer', 
         shape=[hidden_dim, output_dim],
         initializer=tf.random_normal_initializer(stddev=0.1))
