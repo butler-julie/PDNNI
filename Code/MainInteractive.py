8b1ea23722a9a24f97e3d599cc926ca573c009ed
Julie Hartley <51090497+jhartley1121@users.noreply.github.com>
2019-08-20 10:15:26 -0400

    Update MainInteractive.py

--- Code/MainInteractive.py
+++ Code/MainInteractive.py
@@ -11,6 +11,35 @@ import tkinter as tk
 from NeuralNetwork import Trainer, Restore
 
 def command_line():
+  input_file = str(input("Where is the input data located?"))
+  input_dim = int(input("What is the dimension of the input data?"))
+  
+  training_file = str(input("Where is the training data located?"))
+  output_dim = str(input("What is the dimension of the training data?"))
+  
+  hidden_layers = int(input("How many hidden layers?"))
+  hidden_neurons = int(input("How many neurons per hidden layer?"))
+  learning_rate = float(input("What is the learning rate for the training?"))
+  
+  isSavedLosses = bool(input("Save all training losses?"))
+  
+  print ("Setting up the neural network.")
+  
+  train = Trainer (hidden_layers, hidden_neurons, learning_rate, input_dim, output_dim, input_file, training_file, isSavedLosses)
+  
+  iterations = int(input("Number of training iterations?"))
+  
+  print ("Training the neural network.")
+  
+  train.train(iterations)
+  
+  loss = train.get_loss()
+  
+  print ("Neural network saved with final loss of ", loss)
+  
+  
+  
   
   
 def gui ():  
+  print ("GUI")
