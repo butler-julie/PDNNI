d5b5251ab5fe0502975db42606d6f6aaef9dba9e
Julie Lynn Butler <butle222@dev-intel16.i>
2019-08-29 09:45:15 -0400

    generate_subset

--- Code/Support.py
+++ Code/Support.py
@@ -16,6 +16,30 @@
 # get_dims(x): Returns the dimensions of a 2D square array.
 ##################################################
 
+import numpy as np
+from numpy.random import randint
+from math import floor
+
+def generate_subset (input1_name, input2_name, percentage, output1_name, output2_name):
+    input1 = np.load(input1_name)
+    input2 = np.load(input2_name)
+
+    L = len(input1)
+
+    num_points = floor(L * percentage)
+
+    rand = randint(low = 1, high = L-1, size = num_points)
+    rand = np.append(rand, 0)
+    rand = np.append(rand, L-1)
+
+    output1 = input1[rand]
+    output2 = input2[rand]
+
+    np.save(output1_name, output1)
+    np.save(output2_name, output2)
+
+    	
+
 # PRINT_DIMS
 def print_dims (x):
     """
