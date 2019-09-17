cbaca64261f4bf3a02cdd542a822e08d6c578453
Julie Butler <butler@frib.msu.edu>
2019-09-17 17:44:39 -0400

    works for pairing model

--- Code/RNN/rnn_sine.py
+++ Code/RNN/rnn_sine.py
@@ -28,7 +28,7 @@ from keras.layers.recurrent import SimpleRNN
 # The x=values of the training data
 X_train = np.arange(0, 10, 0.01)
 # The value of sin(x) at those points, converted to a list
-y_train = np.sin(X_train).tolist()
+y_train = np.load('H.npy')[0:10000]
 
 # FORMAT_DATA
 def format_data(data, length_of_sequence = 2):  
@@ -53,7 +53,7 @@ def format_data(data, length_of_sequence = 2):
         # Get the element that immediately follows that
         b = data[i+length_of_sequence]
         # Reshape so that each data point is contained in its own array
-        a = np.reshape (a, (len(a), 1))
+        #a = np.reshape (a, (len(a), 1))
         X.append(a)
         Y.append(b)
     rnn_input = np.array(X)
@@ -66,7 +66,7 @@ rnn_input, rnn_training = format_data(y_train, 2)
 print()
 
 def rnn(length_of_sequences, batch_size = None, stateful = False):
-    in_out_neurons = 1
+    in_out_neurons = 36
     hidden_neurons = 100
     inp = Input(batch_shape=(batch_size, 
                 length_of_sequences, 
@@ -102,51 +102,59 @@ plt.title("The final validation loss: {}".format(hist.history["val_loss"][-1]))
 plt.legend()
 plt.show()
 
+def mse (A, B):
+    return np.square(np.subtract(A, B)).mean()
+
 def test_rnn (x1, y_test, plot_min, plot_max):
     X_test, a = format_data (y_test.copy(), 2)
     y_pred = model.predict(X_test)
     plt.figure(figsize=(19,3))
     #plt.plot([10, 10, 10], [1.5, 0, -1.5])
 
-    x1 = x1[0:-2]
+    #x1 = x1[0:-2]
     y_test = y_test[0:-2]
 
-    print(np.square(np.subtract(y_test, y_pred.flatten())).mean())
+    print(np.square(np.subtract(y_test, y_pred)).mean())
     fig, ax = plt.subplots()
-    ax.plot(x1, y_test, label="true", linewidth=2)
-    ax.plot(x1, y_pred, 'g-.',label="predicted", linewidth=2)
-    ax.legend()
+    mse_errors = []
+    for i in range(0, len(y_pred)):
+        mse_errors.append(mse(y_pred[i], y_test[i]))
+        if i%1000 == 0:
+            print(y_test[i])
+            print('*****************************')
 
+    print(len(x1), len(mse_errors))
+    plt.plot(x1, mse_errors, 'r', linewidth=5)
+    plt.xlabel('Flow Parameter')
+    plt.ylabel('MSE from ODE Result')
+    ax.legend()
     ax.axvspan(plot_min, plot_max, alpha=0.25, color='red')
     plt.show()
     
-    diff = y_test - y_pred.flatten()
 
-    plt.plot(x1, diff, linewidth=4)
-    plt.show()
 
-print('X min: 0; X max: 10, dx: 0.001')
-X_test1 = np.arange(0, 10, 0.001)
-y_test1 = np.sin(X_test1)
+#print('X min: 0; X max: 10, dx: 0.001')
+X_test1 = np.arange(0, 50, 0.001)
+y_test1 = np.load('H.npy')
 test_rnn(X_test1, y_test1, 0, 10)
 
-print('X min: -5; X max: 5, dx=0.001')
-X_test2 = np.arange(-5, 5, 0.001)
-y_test2 = np.sin(X_test2)
-test_rnn(X_test2, y_test2, 0, 5)
+#print('X min: -5; X max: 5, dx=0.001')
+#X_test2 = np.arange(-5, 5, 0.001)
+#y_test2 = np.sin(X_test2)
+#test_rnn(X_test2, y_test2, 0, 5)
 
-print('X min: 5; X max: 15, dx=0.001')
-X_test3 = np.arange(5, 15, 0.001)
-y_test3 = np.sin(X_test3)
-test_rnn(X_test3, y_test3, 5, 10)
+#print('X min: 5; X max: 15, dx=0.001')
+#X_test3 = np.arange(5, 15, 0.001)
+#y_test3 = np.sin(X_test3)
+#test_rnn(X_test3, y_test3, 5, 10)
 
-print('X min: -20; X max: 20, dx=0.1')
-X_test4 = np.arange(-20,20, 0.1)
-y_test4 = np.sin(X_test4)
-test_rnn(X_test4, y_test4, 0, 10)
+#print('X min: -20; X max: 20, dx=0.1')
+#X_test4 = np.arange(-20,20, 0.1)
+#y_test4 = np.sin(X_test4)
+#test_rnn(X_test4, y_test4, 0, 10)
 
-print('X min: -100; X max: 100, dx=0.1')
-X_test5 = np.arange(-100,100, 0.1)
-y_test5 = np.sin(X_test5)
-test_rnn(X_test5, y_test5, 0, 10)
+#print('X min: -100; X max: 100, dx=0.1')
+#X_test5 = np.arange(-100,100, 0.1)
+#y_test5 = np.sin(X_test5)
+#test_rnn(X_test5, y_test5, 0, 10)
 
